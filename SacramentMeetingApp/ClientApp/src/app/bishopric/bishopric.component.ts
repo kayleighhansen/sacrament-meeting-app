8a3d6859ca15a7bc8cb493eff91ee790332afd4d
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-02 11:20:43 -0700

    navigation

---
+++ SacramentMeetingApp/ClientApp/src/app/bishopric/bishopric.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bishopric',
+  templateUrl: './bishopric.component.html',
+  styleUrls: ['./bishopric.component.css']
+})
+export class BishopricComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
