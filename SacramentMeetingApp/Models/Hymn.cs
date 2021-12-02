97134f3d893786078ad49796a94be87d9b460972
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-02 13:49:07 -0700

    scaffolding + models

---
+++ SacramentMeetingApp/Models/Hymn.cs
@@ -0,0 +1,10 @@
+
+namespace SacramentMeeting.Models
+{
+    public class Hymn
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
