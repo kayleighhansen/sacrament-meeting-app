e2d257ab54c786165838245287dab17612dc26a1
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-02 13:48:53 -0700

    scaffolding + models

---
+++ SacramentMeetingApp/Models/Bishopric.cs
@@ -0,0 +1,12 @@
+
+
+namespace SacramentMeeting.Models
+{
+    public class Bishopric
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Calling { get; set; }
+        public bool Status { get; set; }
+    }
+}
