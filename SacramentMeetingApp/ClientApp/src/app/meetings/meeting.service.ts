cdcc0860670451e42e0f4d72d80a632f2b6c80ef
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-05 02:18:33 -0700

    ,,,

--- SacramentMeetingApp/ClientApp/src/app/meetings/meeting.service.ts
+++ SacramentMeetingApp/ClientApp/src/app/meetings/meeting.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MeetingService {
 
   constructor() { }
+
+  // how to get the api to deliver data here:
+  
+
 }
