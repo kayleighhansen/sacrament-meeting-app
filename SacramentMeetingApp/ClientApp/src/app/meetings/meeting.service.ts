78b7d4728d16513b8c30ab5c5d91daa71869e56a
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-06 20:06:24 -0700

    get bishopric

--- SacramentMeetingApp/ClientApp/src/app/meetings/meeting.service.ts
+++ SacramentMeetingApp/ClientApp/src/app/meetings/meeting.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Bishopric } from '../bishopric/bishopric.model';
+import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 
 @Injectable({
@@ -6,9 +10,39 @@ import { Injectable } from '@angular/core';
 })
 export class MeetingService {
 
-  constructor() { }
+  bishopric: Bishopric[] = [];
+  bishopricMember: Bishopric;
+
+  fetchBishopricEvent = new Subject<Bishopric[]>();
+  bishopricListChanged = new Subject<Bishopric[]>();
+
+  constructor(private http: HttpClient, 
+              @Inject('BASE_URL') private baseURL: string) { }
 
   // how to get the api to deliver data here:
-  
+  fetchBishopric() {
+    this.http.get<Bishopric>(this.baseURL + "api/Bishoprics")
+    .pipe(
+      map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key})
+          }
+        }
+        return postsArray;
+      })
+    )
+    .subscribe(bishopric => {
+      this.bishopric = bishopric;
+        this.fetchBishopricEvent.next(this.bishopric);
+
+        console.log(this.bishopric);
+
+        this.bishopric.sort((a , b) => 
+        a.status > b.status ? 1 : b.status > a.status ? -1 : 0);
+        this.bishopricListChanged.next(this.bishopric.slice());
+    });
+  }
 
 }
--- SacramentMeetingApp/ClientApp/src/app/meetings/meeting/meeting.component.ts
+++ SacramentMeetingApp/ClientApp/src/app/meetings/meeting/meeting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MeetingService } from '../meeting.service';
 
 @Component({
   selector: 'app-meeting',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeetingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meetingService: MeetingService) { }
 
   ngOnInit() {
+    //this.getBishopricList();
   }
+
 
 }
