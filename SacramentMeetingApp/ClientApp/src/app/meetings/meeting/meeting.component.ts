51e72cff0811502f96c5b2bafa1c63ab7da06a13
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-07 00:21:40 -0700

    ...

--- SacramentMeetingApp/ClientApp/src/app/meetings/meeting/meeting.component.ts
+++ SacramentMeetingApp/ClientApp/src/app/meetings/meeting/meeting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MeetingService } from '../meeting.service';
+import { Meeting } from '../meeting.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-meeting',
@@ -8,11 +10,30 @@ import { MeetingService } from '../meeting.service';
 })
 export class MeetingComponent implements OnInit {
 
+  meeting: Meeting;
+  meetings: Meeting[] = [];
+
+  private meetingChangeSub: Subscription;
+
   constructor(private meetingService: MeetingService) { }
 
   ngOnInit() {
-    //this.getBishopricList();
+    this.meetingService.fetchMeetings();
+    this.meetingChangeSub = this.meetingService.meetingListChanged.subscribe(
+      (meetings: Meeting[]) => {
+        this.meetings = meetings;
+        this.meetings.forEach((meeting) => {
+          this.meeting = meeting;
+        }); 
+      }
+    );
   }
 
+  onSelected() {
 
+  }
+
+  ngOnDestroy(): void {
+    this.meetingChangeSub.unsubscribe();
+  }
 }
