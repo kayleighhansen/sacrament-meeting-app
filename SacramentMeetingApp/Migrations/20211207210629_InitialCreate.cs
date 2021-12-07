bba5de4c3844e646e3edfacf630d6d49181d6c72
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-07 14:31:38 -0700

    ...

---
+++ SacramentMeetingApp/Migrations/20211207210629_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bishopric",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Calling = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bishopric", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meeting",
+                columns: table => new
+                {
+                    MeetingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PresidingId = table.Column<int>(type: "int", nullable: false),
+                    ConductorId = table.Column<int>(type: "int", nullable: false),
+                    OpeningPrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClosingPrayer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OpeningHymnNumberId = table.Column<int>(type: "int", nullable: false),
+                    ClosingHymnNumberId = table.Column<int>(type: "int", nullable: false),
+                    SacramentHymnNumberId = table.Column<int>(type: "int", nullable: false),
+                    IntermediateHymnNumberId = table.Column<int>(type: "int", nullable: true),
+                    DismissalHymnNumberId = table.Column<int>(type: "int", nullable: false),
+                    isFastSunday = table.Column<bool>(type: "bit", nullable: false),
+                    isSpecialMusicNumber = table.Column<bool>(type: "bit", nullable: false),
+                    SpecialMusicNumberName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpecialMusicNumberMusician = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meeting", x => x.MeetingId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Speaker",
+                columns: table => new
+                {
+                    SpeakerId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prefix = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Topic = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MeetingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speaker", x => x.SpeakerId);
+                    table.ForeignKey(
+                        name: "FK_Speaker_Meeting_MeetingId",
+                        column: x => x.MeetingId,
+                        principalTable: "Meeting",
+                        principalColumn: "MeetingId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Speaker_MeetingId",
+                table: "Speaker",
+                column: "MeetingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bishopric");
+
+            migrationBuilder.DropTable(
+                name: "Speaker");
+
+            migrationBuilder.DropTable(
+                name: "Meeting");
+        }
+    }
+}
