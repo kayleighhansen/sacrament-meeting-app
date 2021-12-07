86118d5025cbe3102d7e56c63a9a8efb6f826c41
Kayleigh Hansen <71479913+kayleighhansen@users.noreply.github.com>
2021-12-07 14:30:13 -0700

    ...

--- SacramentMeetingApp/Models/Meeting.cs
+++ SacramentMeetingApp/Models/Meeting.cs
@@ -1,16 +1,19 @@
 
 
+using System.Collections.Generic;
+
 namespace SacramentMeeting.Models
 {
     public class Meeting
     {
-        public long MeetingId { get; set; }
+        public int MeetingId { get; set; }
         public string Date { get; set; }
 
         // People
-        public Bishopric Presiding { get; set; }
-        public Bishopric Conductor { get; set; }
-        public Speaker[] Speakers { get; set; }
+        public int PresidingId { get; set; }
+        
+        public int ConductorId { get; set; }
+        
 
         // Prayers
         public string OpeningPrayer { get; set; }
@@ -18,19 +21,42 @@ namespace SacramentMeeting.Models
 
         // Hymns
         public int OpeningHymnNumberId { get; set; }
+        
 
-        public Hymn OpeningHymnNumber { get; set;}
         public int ClosingHymnNumberId { get; set; }
+        
+        public int SacramentHymnNumberId { get; set; }
+        
+        
+        public int? IntermediateHymnNumberId { get; set; }
+        
+        
+        public int DismissalHymnNumberId { get; set; }
+       
 
-        public Hymn ClosingHymnNumber { get; set; }
-        public int SacramentHymnNumber { get; set; }
-        public int IntermediateHymnNumber { get; set; }
-        public int DismissalHymnNumber { get; set; }
 
         // Special Stuff
         public bool isFastSunday { get; set; }
         public bool isSpecialMusicNumber { get; set; }
         public string SpecialMusicNumberName { get; set; }
         public string SpecialMusicNumberMusician { get; set; }
+
+        /* navigation properties */
+        // public Bishopric Presiding { get; set; }
+
+        // public Bishopric Conductor { get; set; }
+
+        // public Hymn OpeningHymnNumber { get; set;}
+
+        // public Hymn ClosingHymnNumber { get; set; }
+
+        // public Hymn SacramentHymnNumber { get; set; }
+
+        // public Hymn IntermediateHymnNumber { get; set; }
+
+        // public Hymn DismissalHymnNumber { get; set; }
+
+        public virtual ICollection<Speaker> Speakers { get; set; }
+
     }
 }
