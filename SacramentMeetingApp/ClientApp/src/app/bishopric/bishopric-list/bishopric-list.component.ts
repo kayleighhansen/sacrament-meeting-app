6c8e6bd896a4c27c4619b04d7ead1b6233d027c6
Aaron Hathaway <aaronhathaway1@hotmail.com>
2021-12-07 15:09:07 -0700

    Added Subjects/Observables

--- SacramentMeetingApp/ClientApp/src/app/bishopric/bishopric-list/bishopric-list.component.ts
+++ SacramentMeetingApp/ClientApp/src/app/bishopric/bishopric-list/bishopric-list.component.ts
@@ -1,6 +1,7 @@
 import { Route } from '@angular/compiler/src/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { Bishopric } from '../bishopric.model';
 import { BishopricService } from '../bishopric.service';
@@ -10,18 +11,38 @@ import { BishopricService } from '../bishopric.service';
   templateUrl: './bishopric-list.component.html',
   styleUrls: ['./bishopric-list.component.css']
 })
-export class BishopricListComponent implements OnInit {
+export class BishopricListComponent implements OnInit, OnDestroy {
+
 
   bishoprics: Bishopric[];
+  private bishopricChangedSub: Subscription;
+
+  //Can be Deleted Soon
+  private defaultBishopric: Bishopric = {
+    id: 3,
+    name: 'John Taylor',
+    calling: 'Bishop',
+    status: true
+  }
 
   constructor(private bishopricService: BishopricService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.bishoprics = this.bishopricService.getBishoprics();
+
+    this.bishopricChangedSub = this.bishopricService.bishopricChangedEvent.subscribe(
+      (bishoprics: Bishopric[]) => { this.bishoprics = bishoprics }
+    )
   }
 
   onNewBishopric() {
+    this.bishopricService.addBishopric(this.defaultBishopric);
     this.router.navigate(['new'], { relativeTo: this.route })
+  }
+
+
+  ngOnDestroy() {
+    this.bishopricChangedSub.unsubscribe();
   }
 
 }
